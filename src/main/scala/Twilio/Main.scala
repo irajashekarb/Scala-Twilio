5fc61087cbc83ae6f346f2dd43295702c471a77e
Raja Shekar <irajashekarb@gmail.com>
2019-10-13 10:09:42 +0530

    update

--- src/main/scala/Twilio/Main.scala
+++ src/main/scala/Twilio/Main.scala
@@ -8,8 +8,8 @@ import com.typesafe.config.ConfigFactory
 object Main extends App {
   val config = ConfigFactory.load()
 
-  val ACCOUNT_SID = "AC8c9da165e8bb394ef9d130a332fd14c1"
-  val AUTH_TOKEN = "45a1d6e156841a85783993010ed60f72"
+  val ACCOUNT_SID = "Account SID"
+  val AUTH_TOKEN = "Authorization Token"
 
   Twilio.init(ACCOUNT_SID, AUTH_TOKEN)
 
